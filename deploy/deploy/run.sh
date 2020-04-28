ddad6f4d0f1e192cc67fec1b55a1a3def79e4532
khudikea <EKhudik@qulix.com>
2020-04-28 16:10:53 +0300

    copy bundle to deploy container for linux host

--- deploy/deploy/run.sh
+++ deploy/deploy/run.sh
@@ -7,4 +7,6 @@ docker build -f deploy.dockerfile -t tizen-deploy .
 baseDir = $(pwd)
 baseDir = "$(dirname $baseDir)"
 
+yes | cp "$pwd/../../src/tizen-client/dist/*.*" ./dist
+
 docker run --network=host  -v $baseDir/certificates:/home/tizen/SamsungCertificate/ea_cert:ro -it -e TV_IP=$TV_IP tizen-deploy
