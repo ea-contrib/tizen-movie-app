8b66f4f49f1508de03a6eed73669a9cbe9989ba2
Uladzislau Zubtsou <uzubtsou@outlook.com>
2020-05-13 12:43:23 +0300

    Docker styling change

--- deploy/create_certificate/run.sh
+++ deploy/create_certificate/run.sh
@@ -4,4 +4,9 @@
 
 docker build -f createcertificate.dockerfile -t tizen-certificate .
 
-docker run --network=host  -v $(dirname $(pwd))/certificates:/home/tizen/SamsungCertificate/ea_cert -v /tmp/.X11-unix:/tmp/.X11-unix --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -it -e DISPLAY=$DISPLAY tizen-certificate
+docker run -it --network=host  \
+	-v $(dirname $(pwd))/certificates:/home/tizen/SamsungCertificate/ea_cert \
+	-v /tmp/.X11-unix:/tmp/.X11-unix 
+	-v "$HOME/.Xauthority:/root/.Xauthority:rw" 
+	-e DISPLAY=$DISPLAY
+	tizen-certificate
