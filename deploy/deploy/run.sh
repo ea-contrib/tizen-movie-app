9c1ccbed0f195e22796f5246adadb6c19a5c87dd
khudikea <EKhudik@qulix.com>
2020-05-15 20:00:40 +0300

    fir for base dir

--- deploy/deploy/run.sh
+++ deploy/deploy/run.sh
@@ -6,7 +6,4 @@ yes | cp "$pwd/../../src/tizen-client/dist/*.*" ./dist
 
 docker build -f deploy.dockerfile -t tizen-deploy .
 
-baseDir = $(pwd)
-baseDir = "$(dirname $baseDir)"
-
-docker run --network=host  -v $baseDir/certificates:/home/tizen/SamsungCertificate/ea_cert:ro -it -e TV_IP=$TV_IP tizen-deploy
+docker run --network=host  -v $(dirname $(pwd))/certificates:/home/tizen/SamsungCertificate/ea_cert:ro -it -e TV_IP=$TV_IP tizen-deploy
