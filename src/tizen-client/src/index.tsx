44900af90d8a91ea354e0881cd8e55395b44641c
khudikea <EKhudik@qulix.com>
2020-05-15 20:23:00 +0300

    router changed to in memory

--- src/tizen-client/src/index.tsx
+++ src/tizen-client/src/index.tsx
@@ -1,16 +1,16 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 
 import { default as App } from "./src/App";
 import { store } from "./src/Store";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <MemoryRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </MemoryRouter>,
   document.querySelector(".app")
 );
