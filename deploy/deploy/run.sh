7721b67def753ff6e87d74abca65f1013e82dd5a
khudikea <EKhudik@qulix.com>
2020-05-15 19:59:16 +0300

    changed order of deploy commands

--- deploy/deploy/run.sh
+++ deploy/deploy/run.sh
@@ -2,11 +2,11 @@
 
 ./env.sh
 
+yes | cp "$pwd/../../src/tizen-client/dist/*.*" ./dist
+
 docker build -f deploy.dockerfile -t tizen-deploy .
 
 baseDir = $(pwd)
 baseDir = "$(dirname $baseDir)"
-
-yes | cp "$pwd/../../src/tizen-client/dist/*.*" ./dist
 
 docker run --network=host  -v $baseDir/certificates:/home/tizen/SamsungCertificate/ea_cert:ro -it -e TV_IP=$TV_IP tizen-deploy
